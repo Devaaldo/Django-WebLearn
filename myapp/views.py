2c7ba8aaa8447b462a3b818afac990440d438d63
Devaaldo <akbaraja551@gmail.com>
2024-11-04 23:41:30 +0700

    web django

---
+++ myapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import About
+from django.shortcuts import render
+from .models import About, Project, Contact
+from .forms import FormContact
+
+# Create your views here.
+
+def index(request):
+    about = About.objects.get(id=1)
+    projects = Project.objects.all()
+    context = {
+        'about': about,
+        'projects': projects,
+        'form': FormContact()
+    }
+    return render(request, 'index.html', context)
+
+def send_contact(request):
+    if request.POST:
+        form = FormContact(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/#contact')
+    return redirect('/#contact')
